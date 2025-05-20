25cf3b40cb116e0a94510c563bb364dc32b08fe9
DebitGurung <140620224+Yellowhom@users.noreply.github.com>
2025-05-21 00:01:28 +0545

    SecondCommit dbcontext

---
+++ KitabhChautari.Api/Data/KitabhChautariDBContext.cs
@@ -0,0 +1,46 @@
+﻿using KitabhChauta.Model;
+using KitabhChautari.Api.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+namespace KitabhChautari.Api.Data
+{
+    public class KitabhChautariDBContext : DbContext
+    {
+        private readonly IPasswordHasher<User> _passwordHasher;
+        public KitabhChautariDBContext(DbContextOptions<KitabhChautariDBContext> options, IPasswordHasher<User> passwordHasher) : base(options)
+        {
+            _passwordHasher = passwordHasher;
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Author> authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var adminUser = new User
+            {
+                Id = 1,
+                Name = "Noel Prince",
+                Email = "noel@gmail.com",
+                Phone = "9817108031",
+                Role = nameof(UserRole.Admin),
+                IsApproved = true,
+            };
+
+            adminUser.PasswordHash = _passwordHasher.HashPassword(adminUser, "9817108031");
+            modelBuilder.Entity<User>(entity =>
+            {
+            }
+    }
+    } 
+}
