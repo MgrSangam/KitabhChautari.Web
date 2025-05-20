76ab5968079208a7591db9f0f9a641b85de3fd1d
Sujal Gurung <sujalgurn@gmail.com>
2025-05-20 23:44:37 +0545

    admin class

---
+++ KitabhChautari.Api/Data/Entities/Admin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KitabhChautari.Api.Data.Entities
+{
+    public class Admin
+    {
+
+        public int AdminId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; } = nameof(UserRole.Admin);
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string PasswordHash { get; set; }
+
+    }
+}
