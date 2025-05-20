9049f2de18ebb0c340908b57fc3432cb2d0956f3
Sujal Gurung <sujalgurn@gmail.com>
2025-05-21 00:11:01 +0545

    /

--- KitabhChautari.Api/Program.cs
+++ KitabhChautari.Api/Program.cs
@@ -1,9 +1,16 @@
+using KitabhChautari.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<KitabhChautariDBContext>(options =>
+{
+    options.pg
+})
 
 var app = builder.Build();
 
